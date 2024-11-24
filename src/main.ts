91df90773ff8c459e8aa7aa29722b1913c90f687
NinYuri <yurianamontserrath2003@gmail.com>
2024-11-24 01:35:53 -0600

    Gateway finalizado

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { envs } from './config';
+
+async function bootstrap() 
+{
+  const looger = new Logger('Main-Gateway');
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
+
+  await app.listen(envs.port);
+  looger.log(`GateWay Corriendo en el Puerto #${envs.port}`);
+}
+bootstrap();
---
+++ src/products/products.controller.ts
@@ -0,0 +1,34 @@
+import { Body, Controller, Delete, Get, Inject, Param, Patch, Post, Query } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { PaginationDto } from 'src/common';
+import { PRODUCT_MICROSERVICE } from 'src/config';
+
+@Controller('products')
+export class ProductsController {
+  constructor( @Inject(PRODUCT_MICROSERVICE) private readonly productsClient: ClientProxy ) {}
+
+  @Post()
+  createProduct() {
+    return 'Crea un producto';
+  }
+
+  @Get()
+  findAllProducts(@Query() paginationDto: PaginationDto) {
+    return this.productsClient.send({cmd: 'find_all_products'}, paginationDto);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id:string) {
+    return 'Esta función regresa el producto #:' + id;
+  }
+
+  @Patch(':id')
+  patchProduct(@Param('id') id:string, @Body() body:any) {
+    return 'Esta función actualiza el producto #:' + id;
+  }
+
+  @Delete(':id')
+  deleteProduct(@Param('id') id:string) {
+    return 'Esta función elimina el producto #:' + id;
+  }
+}
